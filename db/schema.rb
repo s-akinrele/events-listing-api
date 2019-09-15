812a7582e5f4172979241c38b6f0f74a8475de4d
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2019-09-15 21:08:48 +0100

    add user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_190848) do
+ActiveRecord::Schema.define(version: 2019_09_15_195950) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2019_09_15_190848) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.text "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

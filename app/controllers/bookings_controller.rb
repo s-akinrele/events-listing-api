1f58f84c73102430fa1e4a34b9095a318c91702c
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2019-09-15 23:17:49 +0100

    add bookings

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,13 @@
+class BookingsController < ApplicationController
+
+  def create
+    booking = Booking.create!(bookings_params)
+    json_response(booking, :created)
+  end
+
+  private
+
+  def bookings_params
+    params.permit(:user_id, :event_id)
+  end
+end
